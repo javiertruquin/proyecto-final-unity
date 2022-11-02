849ab08be7f77072ec8116bbb6f3d114579e2814
javiertruquin <javiertruquin@gmail.com>
2022-11-02 00:25:56 -0300

    Animaciones, Camara y Timer
    Muchas cosas , entre animacion para todos lados, camara 1ra 3ra y panoramica, y timer para finalizar partida

--- Assets/Scripts/AnimacionesMov.cs
+++ Assets/Scripts/AnimacionesMov.cs
@@ -45,4 +45,32 @@ public class AnimacionesMov : MonoBehaviour
     //         anim.SetFloat("Speed Backward",speedback);
     //         anim.SetFloat("Speed",vel);
     // }
+
+//     using UnityEngine;
+
+// public class AnimacionesMov : MonoBehaviour
+// {
+
+//     public float velocidadMovimiento=5.0f;
+//     public float velocidadRotacion = 200.0f;
+//     private Animator anim;
+//     public float x,y;
+
+//     void Start()
+//     {
+//         anim = GetComponent<Animator>();
+//     }
+
+//     void Update()
+//     {
+//         x = Input.GetAxis("Horizontal");
+//         y = Input.GetAxis("Vertical");
+
+//         transform.Rotate(0,x*Time.deltaTime * velocidadRotacion, 0);
+//         transform.Translate(0,0,y*Time.deltaTime * velocidadMovimiento);
+
+//         anim.SetFloat("VelX",x);
+//         anim.setFloat("VelY",y);
+//     }
+// }
 }
